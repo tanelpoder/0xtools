9b1dd97ef5e01b884dc4568120a560ea28b03f08
Tanel Poder <tanel@tanelpoder.com>
2021-03-15 17:10:39 -0400

    update license to GPL2.0 or later

--- include/syscall_names.h
+++ include/syscall_names.h
@@ -1,26 +1,30 @@
 /* 
- * 0x.Tools by Tanel Poder [https://0x.tools]
- * Copyright 2019-2020 Tanel Poder
+ *  0x.Tools xCapture - sample thread activity from Linux procfs [https://0x.tools]
+ *  Copyright 2019-2021 Tanel Poder
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * full architecture-specific list linux kernel source: 
- * arch/x86/entry/syscalls/syscall_64.tbl 
- * or run ausyscall --dump or parse unistd.h
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *  awk '/^[0-9]/{ printf("[%4d] = {\"%s\", \"%s\"},\n", $1, $2, $3); }' < ./arch/x86/entry/syscalls/syscall_64.tbl
+ *  SPDX-License-Identifier: GPL-2.0-or-later 
+ *
+ * NOTES:
+ *
+ *  full architecture-specific list linux kernel source: 
+ *  arch/x86/entry/syscalls/syscall_64.tbl 
+ *  or run ausyscall --dump or parse unistd.h
+ *
+ *   awk '/^[0-9]/{ printf("[%4d] = {\"%s\", \"%s\"},\n", $1, $2, $3); }' < ./arch/x86/entry/syscalls/syscall_64.tbl
  */
 
 typedef struct sysent {
--- src/xcapture.c
+++ src/xcapture.c
@@ -1,19 +1,22 @@
 /* 
- * 0x.Tools by Tanel Poder [https://0x.tools]
- * Copyright 2019-2020 Tanel Poder
+ *  0x.Tools xCapture - sample thread activity from Linux procfs [https://0x.tools]
+ *  Copyright 2019-2021 Tanel Poder
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  SPDX-License-Identifier: GPL-2.0-or-later 
  *
  */
 
