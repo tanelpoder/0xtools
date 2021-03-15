719691bf0b76d73056885496f622fcb9c8a61238
Tanel Poder <tanel@tanelpoder.com>
2021-03-15 18:14:26 -0400

    gpl2.0

--- lib/proc.py
+++ lib/proc.py
@@ -1,9 +1,9 @@
-#  proc.py -- Linux Process Snapper by Tanel Poder
-#  Copyright 2019 Tanel Poder
-#
-#  This program is free software: you can redistribute it and/or modify
+#  psn -- Linux Process Snapper by Tanel Poder [https://0x.tools]
+#  Copyright 2019-2021 Tanel Poder
+#  
+#  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
+#  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
 #  
 #  This program is distributed in the hope that it will be useful,
@@ -11,8 +11,11 @@
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #  
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#  
+#  SPDX-License-Identifier: GPL-2.0-or-later 
 
 # structures defining /proc
 import os, os.path
